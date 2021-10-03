3231556cef1103e11a987f9323a4f79ef3ab7359
simplysamiul <simplysamiul@gmail.com>
2021-10-03 08:46:49 +0600

    added inventory and final code

--- src/Component/Inventory/Inventory.js
+++ src/Component/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h1>This is inventory !</h1>
+            <h1>Inventory Coming Soon !!</h1>
         </div>
     );
 };
